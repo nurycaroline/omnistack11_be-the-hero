c5123bfaffb5c185437c360a505564f86cc08933
Nurielly Caroline Brizola <nurycaroline@gmail.com>
2020-03-27 16:55:06 -0300

    Rota de loginÏ

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,6 +5,7 @@ const routes = express.Router();
 const OngController = require("./controller/OngController");
 const IncidentController = require("./controller/IncidentController");
 const ProfileController = require("./controller/ProfileController");
+const SessionController = require("./controller/SessionController");
 
 routes.get("/ongs", OngController.index);
 routes.post("/ongs", OngController.create);
@@ -14,5 +15,7 @@ routes.post("/incidents", IncidentController.create);
 routes.delete("/incidents/:id", IncidentController.delete);
 
 routes.get("/profile", ProfileController.index);
+
+routes.post("/session", SessionController.create);
 
 module.exports = routes;
