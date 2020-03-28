fec05d082f7cd0d99b6e8342e8a8609423663e67
Nurielly Caroline Brizola <nurycaroline@gmail.com>
2020-03-28 10:58:48 -0300

    Testes no Backend

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -12,35 +12,46 @@ module.exports = {
     useNullAsDefault: true
   },
 
-  staging: {
-    client: "postgresql",
+  test: {
+    client: "sqlite3",
     connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
-    pool: {
-      min: 2,
-      max: 10
+      filename: "./src/database/test.sqlite"
     },
     migrations: {
-      tableName: "knex_migrations"
-    }
+      directory: "./src/database/migrations"
+    },
+    useNullAsDefault: true
   },
 
-  production: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: "knex_migrations"
-    }
-  }
+  // staging: {
+  //   client: "postgresql",
+  //   connection: {
+  //     database: "my_db",
+  //     user: "username",
+  //     password: "password"
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: "knex_migrations"
+  //   }
+  // },
+
+  // production: {
+  //   client: "postgresql",
+  //   connection: {
+  //     database: "my_db",
+  //     user: "username",
+  //     password: "password"
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: "knex_migrations"
+  //   }
+  // }
 };
--- backend/src/app.js
+++ backend/src/app.js
@@ -16,4 +16,4 @@ app.use(express.json());
 app.use(routes);
 app.use(errors()); // last use
 
-app.listen(3333);
+module.exports = app;
