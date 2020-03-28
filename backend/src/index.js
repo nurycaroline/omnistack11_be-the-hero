73c3ddd887a0d50703be97bf612583a4e1b3ad96
Nurielly Caroline Brizola <nurycaroline@gmail.com>
2020-03-28 09:43:42 -0300

    Validacao de parametros

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,19 @@
-const cors = require("cors");
 const express = require("express");
+const cors = require("cors");
+const { errors } = require("celebrate");
 const routes = require("./routes");
 
 const app = express();
 
-app.use(cors());
 // production:
 // app.use(
 //   cors({
 //     origin: "https://myapp.com.br"
 //   })
 // );
+app.use(cors()); // first use
 app.use(express.json());
 app.use(routes);
+app.use(errors()); // last use
 
 app.listen(3333);
